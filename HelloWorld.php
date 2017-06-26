1596a8f974857fac532b70357b830aa872740e57
Bhanu <as.bhanuprakash@gmail.com>
2017-06-26 19:18:28 +0530

    Add HelloWorld class

---
+++ HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Bhanu
+ * Date: 26/06/2017
+ * Time: 19:11
+ */
+class HelloWorld
+{
+    public function sayHello()
+    {
+        echo 'Hello World.';
+    }
+}
